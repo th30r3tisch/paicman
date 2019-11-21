5dbde84cf6e4f8e28cf920b35245fef326bb43bc
th30r3tisch <theoretisch@hotmail.de>
2019-11-21 18:34:49 +0100

    added login screen

--- frontend/src/World.java
+++ frontend/src/World.java
@@ -1,8 +1,18 @@
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -11,11 +21,39 @@ import javafx.scene.shape.*;
 public class World extends Application {
 
     private ArrayList<Shape> nodes;
+    private Scene login, game;
 
     @Override public void start(Stage primaryStage) {
-        primaryStage.setTitle("Drag circles around to see collisions");
+        primaryStage.setTitle("Conquer all towns");
+
+        //login scene
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("Nick name:");
+        grid.add(userName, 0, 1);
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Button startbtn = new Button("Start");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(startbtn);
+        grid.add(hbBtn, 1, 4);
+        startbtn.setOnAction(e -> primaryStage.setScene(game));
+
+        login = new Scene(grid, 300, 275);
+
+        //game scene
         Group root = new Group();
-        Scene scene = new Scene(root, 400, 400);
+        game = new Scene(root, 400, 400);
 
         nodes = new ArrayList<>();
         nodes.add(new Circle(15, 15, 30));
@@ -27,7 +65,7 @@ public class World extends Application {
         root.getChildren().addAll(nodes);
         checkShapeIntersection(nodes.get(nodes.size() - 1));
 
-        primaryStage.setScene(scene);
+        primaryStage.setScene(login);
         primaryStage.show();
     }
 
