0fec06e2ef41c337427ced268ae23b8f99c5b44c
th30r3tisch <theoretisch@hotmail.de>
2019-12-03 23:13:38 +0100

    first simple tcp connection backend

---
+++ backend/src/game/model/Player.java
@@ -0,0 +1,16 @@
+package game.model;
+
+import java.io.Serializable;
+
+public class Player implements Serializable {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
