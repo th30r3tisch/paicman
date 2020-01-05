14a380035b12890dae226f1660cdccc3939cca8f
Eko <e.supardjo@gmx.de>
2020-01-05 21:10:47 +0100

    init new ui

--- frontend/src/game/view/World.java
+++ frontend/src/game/view/World.java
@@ -1,25 +1,24 @@
 package game.view;
 import game.controller.WorldController;
 import javafx.animation.AnimationTimer;
+import javafx.geometry.Orientation;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 public class World implements ViewInterface {
 
     private Scene world;
     private WorldController controller;
     private int windowWidth = 800;
-    private int windowHeight = 1200;
+    private int windowHeight = 600;
     private int gameMapHeight = 2000;
     private int gameMapWidth = 4000;
     private Image backgroundImage;
@@ -43,28 +42,45 @@ public class World implements ViewInterface {
                 controller.getObjects()
         );
 
-        Menu menu = new Menu("Menu");
-        MenuBar menubar = new MenuBar();
-        menubar.getMenus().add(menu);
+
 
         BorderPane borderPane = new BorderPane();
-        borderPane.setTop(menubar);
+        borderPane.setTop(getMenu());
 
-        ImageView background = new ImageView(backgroundImage);
-        background.setFitHeight(gameMapHeight);
-        background.setFitWidth(gameMapWidth);
+
+        ImageView imageView = new ImageView(backgroundImage);
+        imageView.setFitHeight(gameMapHeight);
+        imageView.setFitWidth(gameMapWidth);
+
+
+        SplitPane splitPane = new SplitPane();
+        splitPane.setOrientation(Orientation.HORIZONTAL);
+        //splitPane.setDividerPosition(100, 100);
+        splitPane.getItems().add(getInfoBox());
+        //splitPane.getItems().add(gamePane);
+
+        AnchorPane anchorPane1 = new AnchorPane();
 
         StackPane stack = new StackPane();
-        stack.getChildren().setAll(
-                background,
+        stack.getChildren().setAll(imageView);
+        ScrollPane scroll = controller.createScrollPane(stack);
+        splitPane.getItems().add(scroll);
+
+        AnchorPane rootAnchor = new AnchorPane();
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(
                 borderPane,
-                g
+                splitPane
+
+        );
+        rootAnchor.getChildren().addAll(
+                vBox
         );
 
         // wrap the scene contents in a pannable scroll pane.
-        ScrollPane scroll = controller.createScrollPane(stack);
 
-        world = new Scene(scroll, windowWidth, windowHeight);
+
+        world = new Scene(rootAnchor, windowWidth, windowHeight);
 
         // bind the preferred size of the scroll area to the size of the scene.
         scroll.prefWidthProperty().bind(world.widthProperty());
@@ -83,5 +99,54 @@ public class World implements ViewInterface {
         };
         timer.start();
         return world;
+    }
+
+    //infobox of current player status
+    private AnchorPane getInfoBox(){
+        AnchorPane parent = new AnchorPane();
+        VBox infoBox = new VBox();
+        Text hitPointText = new Text();
+        //exampleText
+        hitPointText.setText("Villages: 20");
+
+        Text numSoldierText = new Text();
+        numSoldierText.setText("Number of Soldiers: 10");
+
+        infoBox.getChildren().addAll(
+                hitPointText,
+                numSoldierText
+        );
+        parent.getChildren().add(infoBox);
+
+        return  parent;
+    }
+
+    //infobox of villages
+    //should be called on click
+    private VBox detailInfoBox(){
+        VBox infoBox = new VBox();
+        return infoBox;
+    }
+
+    private ScrollPane getGameMap(){
+        ScrollPane scrollPane = new ScrollPane();
+        Text hitPointText = new Text();
+        //exampleText
+        hitPointText.setText("hitPoint: 10");
+
+        Text numSoldierText = new Text();
+        numSoldierText.setText("Number of Soldiers: 10");
+        return scrollPane;
+    }
+
+    private MenuBar getMenu(){
+        Menu menu = new Menu("Menu");
+        MenuItem menuItem1 = new MenuItem();
+        menuItem1.setText("test");
+        menu.getItems().add(menuItem1);
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().add(menu);
+
+        return  menuBar;
     }
 }
